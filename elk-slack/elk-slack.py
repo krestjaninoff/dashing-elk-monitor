d7ad0c2e2c78daeab4a0682e1e3ec7480480faf1
Mikhail Krestyaninov <mikhail.krestyaninov@jetbrains.com>
2016-08-09 19:00:27 +0300

    Message formatting

--- elk-slack/elk-slack.py
+++ elk-slack/elk-slack.py
@@ -54,10 +54,10 @@ errors = client.search(index=index, body=es_request, size=5)
 # Send message to Slack
 for error in errors['hits']['hits']:
 
-    message = (error["_source"]["component"] if 'component' in error["_source"] else "python") + ": " + \
+    message = "*" + (error["_source"]["component"] if 'component' in error["_source"] else "python") + "*: " + \
               error["_source"]["message"]
     if 'stack_trace' in error["_source"]:
-        message += "\n\n" + error["_source"]["stack_trace"][:500]
+        message += "\n\n```" + error["_source"]["stack_trace"][:500] + "```"
 
     #print(message)
     slack.post({"text": message})
